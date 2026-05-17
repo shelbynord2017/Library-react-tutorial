59c6f0d166f4d6bfa023680dab7ea73d316535e3
Shelbynord2017 <shelbynord2017@gmail.com>
2026-05-16 22:26:32 -0400

    finished footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import Nav from "./components/Nav";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Landing from "./components/Landing";
+import Highlights from "./components/Highlights";
+import Featured from "./components/Featured"
+import Discounted from "./components/Discounted"
+import Explore from "./components/Explore"
+import Footer from "./components/Footer"
 
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
+      <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
