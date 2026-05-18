3ed52497cef518dd8049779e957db4c8c8c325f4
Shelbynord2017 <shelbynord2017@gmail.com>
2026-05-17 22:34:07 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from "./pages/Home"
 import Books from "./pages/Books";
 import { books } from "./data.js"
+import BookInfo from "./pages/BookInfo"
 
 
 
@@ -15,6 +16,7 @@ function App() {
         <Nav />
         <Route path="/" exact component={Home} />
         <Route path="/books" render={() => <Books books={books} />} />
+        <Route path="/books/1" render={() => <BookInfo books={books} />} />
         <Footer />
       </div>
     </Router>
